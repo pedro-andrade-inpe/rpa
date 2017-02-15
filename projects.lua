5185edbeeea4683b2cf47eb93df6b48deef90553
Pedro Andrade <pedro.andrade@inpe.br>
2017-02-15 09:15:52 -0200

    Small updates in the names of the files.

--- projects.lua
+++ projects.lua
@@ -2,9 +2,9 @@
 import("terralib")
 
 project = Project{
-    file = "bau.tview",
-    clean = true,
+	file = "bau.tview",
+	clean = true,
 	biomes = "biomas_br_pol.shp",
 	states = "estados_br_pol.shp",
-	bau = "bau.shp"
+	bau = "BAU.shp"
 }
