17042ff8a01fbf2080a5c4648d227e8a883e9bb7
Pedro Andrade <pedro.andrade@inpe.br>
2018-01-08 12:59:35 -0200

    Updates to the application.

--- redd-pac.lua
+++ redd-pac.lua
@@ -1,27 +1,20 @@
 
+-- run projects.lua first
+
 import("publish")
 
-local report = Report{
-	title = "BAU",
-	author = "Camara et. al (2015)"
-}
-
-report:addHeading("Introduction")
-
 Application{
-    project = "bau.tview",
+	project = "bau.tview",
 	title = "BAU Scenario",
-    description = "Results of BAU (Business As Usual) scenario of Redd PAC project. For more information, please visit www.redd-pac.org.",
-    clean = true,
-    output = "BAUWebMap",
---	report = report,
+	description = "Results of BAU (Business As Usual) scenario of Redd PAC project. For more information, please visit www.redd-pac.org.",
+	clean = true,
+	output = "BAUWebMap",
 	order = {"biomes", "states", "bau"},
 	biomes = View{
 		select = "name",
 		color = "Set2",
 		width = 1,
 		description = "Brazilian Biomes, from IBGE.",
--- [[
 		report = function(cell)
 			_Gtme.print("processing report for "..cell.name)
 			local mreport = Report{
@@ -32,14 +25,14 @@ Application{
 			mreport:addText("Abc "..cell.name)
 			return mreport
 		end
---]]
 	},
 	states  = View{
 		color = "yellow",
 --		select = "NOMEUF2",
 		width = 1,
 		description = "States",
---[[		report = function(cell)
+--[[
+		report = function(cell)
 			local mreport = Report{
 				title = cell.NOMEUF2,
 				author = "REDD PAC Project"
@@ -50,20 +43,20 @@ Application{
 		end
 --]]
 	},
-    bau = View{ -- try a wrong layer name and get a bad error message
-        title = "BAU",
-        description = "BAU results",
-        width = 1,
+	bau = View{ -- try a wrong layer name and get a bad error message
+		title = "BAU",
+		description = "BAU results",
+		width = 1,
 		--select = "Defor2020",
 
 	-- only work with categories, and not with slices. implement it
 --[[
-        select = "TotFor2050",
+		select = "TotFor2050",
 		min = 0,
 		max = 310,
 		slices = 10,
 --]]
 		color = "red"
-    }
+	}
 }
 
